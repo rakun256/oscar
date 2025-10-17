0832aee693cc52f2c26a3bac77380b6b09f4890f
Emre Uslu <emre.uslu@d-teknoloji.com.tr>
2025-10-17 22:23:18 +0300

    feat: integrate ProtectedRoute within AuthProvider for route protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { UIProvider } from "./context/UIContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
     <>
       <UIProvider>
-        <AuthProvider></AuthProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* Public Routes */}
+              <Route />
+              {/* Protected Routes */}
+              <Route element={<ProtectedRoute />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
       </UIProvider>
     </>
   );
