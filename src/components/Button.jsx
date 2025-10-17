da24ebc9a9ac32390b2528f0135602f82dac9108
Emre Uslu <emre.uslu@d-teknoloji.com.tr>
2025-10-17 23:10:03 +0300

    feat: implement Button component and update ComponentTest page with button examples

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+export default function Button({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  disabled = false,
+  className = "",
+}) {
+  const baseStyle =
+    "border-2 px-6 py-2 rounded-2xl  font-semibold transition cursor-pointer";
+
+  const variants = {
+    primary: "border-primary text-primary hover:bg-primary hover:text-bg",
+    secondary:
+      "border-secondary text-secondary hover:bg-secondary hover:text-bg",
+    outline_primary:
+      "border-primary text-bg hover:bg-bg hover:text-primary bg-primary",
+    outline_secondary:
+      "border-secondary text-bg hover:bg-bg hover:text-secondary bg-secondary",
+  };
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${baseStyle} ${variants[variant]} ${
+        disabled
+          ? "opacity-50 cursor-not-allowed! hover:bg-bg! hover:text-primary"
+          : ""
+      } ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
--- src/pages/Test/ComponentTest.jsx
+++ src/pages/Test/ComponentTest.jsx
@@ -1,3 +1,31 @@
+import Button from "../../components/Button";
+import { useUI } from "../../context/UIContext";
+
 export default function ComponentTest() {
-  return <div>Component Test Page</div>;
+  const { notify } = useUI();
+
+  const handleClick = () => {
+    notify.success("Button clicked!");
+  };
+
+  return (
+    <div className="flex flex-col w-[100] min-h-[100vh] items-center justify-center">
+      <h1 className="text-text font-bold text-2xl ">Component Test Page</h1>
+      <div className="flex flex-col gap-3 p-6">
+        <Button onClick={handleClick}>Primary Button</Button>
+        <Button onClick={handleClick} variant="secondary">
+          Secondary Button
+        </Button>
+        <Button onClick={handleClick} variant="outline_primary">
+          Outline Primary Button
+        </Button>
+        <Button onClick={handleClick} variant="outline_secondary">
+          Outline Secondary Button
+        </Button>
+        <Button disabled variant="primary">
+          Disabled Button
+        </Button>
+      </div>
+    </div>
+  );
 }
