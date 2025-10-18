bb2b62e6923d0c599dd271e33c4d95aafdbcc823
Emre Uslu <emreuslu256@gmail.com>
2025-10-18 14:13:46 +0300

    Button upgraded

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({
   className = "",
 }) {
   const baseStyle =
-    "border-2 px-6 py-2 rounded-2xl  font-semibold transition cursor-pointer";
+    "border-2 px-6 py-2 rounded-2xl  font-semibold transition cursor-pointer active:scale-95";
 
   const variants = {
     primary: "border-primary text-primary hover:bg-primary hover:text-bg",
@@ -26,7 +26,7 @@ export default function Button({
       disabled={disabled}
       className={`${baseStyle} ${variants[variant]} ${
         disabled
-          ? "opacity-50 cursor-not-allowed! hover:bg-bg! hover:text-primary"
+          ? "opacity-50 cursor-not-allowed! hover:bg-bg! hover:text-primary active:scale-100!"
           : ""
       } ${className}`}
     >
