cb94ee343ab524189ff0f8a83eaf95dd6dc0f4b4
Emre Uslu <emreuslu256@gmail.com>
2025-10-18 20:46:21 +0300

    feat: enhance WelcomePage layout and add application logo

--- src/pages/Auth/Welcome.jsx
+++ src/pages/Auth/Welcome.jsx
@@ -1,23 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
+import logo from "../../assets/app_icon_purple.png";
 
 export default function WelcomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-bg text-text animate-fade-in px-6">
-      <div className="mb-8 text-center">
+    <div className="flex flex-col items-center justify-around md:justify-center min-h-screen bg-bg text-text animate-fade-in px-6 md:gap-12">
+      <div className="text-center">
         <h1 className="text-4xl font-extrabold tracking-tight text-primary mb-2">
           Oscar
         </h1>
-        <p className="text-text-secondary text-base font-medium max-w-sm">
-          Küçük topluluklar için görev paylaşımını kolaylaştır. Herkes görevini
-          yaptığında, tribe huzurlu olur 💫
+        <p className="text-text-secondary text-sm md:text-lg font-medium">
+          Küçük topluluklar için görev paylaşımını kolaylaştır. <br /> Herkes
+          görevini yaptığında, tribe huzurlu olur 💫
         </p>
       </div>
 
-      <div className="w-40 h-40 mb-10 rounded-full bg-accent/30 flex items-center justify-center text-accent text-6xl font-bold shadow-inner">
-        🐾
+      <div className="w-40 h-40 rounded-full bg-accent/30 flex items-center justify-center text-accent text-6xl font-bold shadow-inner">
+        <img src={logo} alt="Application Logo" className="w-32 h-32" />
       </div>
 
       <div className="flex flex-col gap-4 w-full max-w-xs">
@@ -38,7 +39,7 @@ export default function WelcomePage() {
         </Button>
       </div>
 
-      <footer className="mt-10 text-xs text-text-secondary opacity-80">
+      <footer className="md:absolute md:bottom-0 text-sm text-text-secondary opacity-80 md:bg-accent w-full py-5 text-center">
         <p>© 2025 Oscar • Birlikte başarmak güzeldir 🌱</p>
       </footer>
     </div>
